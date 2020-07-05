3712b25772d879c7df3d50de2ebf38302ba5cd2f
yashu762001 <67051799+yashu762001@users.noreply.github.com>
2020-07-05 22:47:07 +0530

    Update PowerSum.java

--- PowerSum.java
+++ PowerSum.java
@@ -11,8 +11,14 @@ static int powerSum(int X, int N, int p) {
 
         else {
             return 0 ; 
-        } 
-
-        
-
+        }      
     }
+
+// This is just a function. You would have to give input on your own by creatin gmain function.
+
+// The Logic of this problem is to keep choosing numbers. That is either i will choose 1 or not then if i won't choose i will call for 2 and apply same previous condition.
+// If i will choose 1 then the number will decrease by 1 and i will call the same function with changed value of X and check for 2. 
+
+// Like wise if p^N<X we will return what is defined above.
+// if p^N==X then only 1 number exists to bring this sum thus we returned 1.
+// Else in remaining case we will return 0 since such a positive number won'y exist.
